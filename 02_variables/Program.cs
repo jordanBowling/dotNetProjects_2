be176ddf4bd91beffaf9e4960ffc7ababfa30931
Jordan Bowling <jordanbowlingvfx@gmail.com>
2021-09-10 07:43:54 -0400

    Added Types

--- 02_variables/Program.cs
+++ 02_variables/Program.cs
@@ -10,6 +10,21 @@ namespace _02_variables
     {
         static void Main(string[] args)
         {
+            string firstName = "Paul";
+            Console.WriteLine(firstName);
+            Console.ReadLine();
+
+            string lastName = "Johnson";
+            int birthYear;
+
+            Console.WriteLine(lastName);
+            Console.ReadLine();
+           
+
+            birthYear = 1990;
+            Console.WriteLine(birthYear);
+            Console.ReadLine();
         }
+
     }
 }
