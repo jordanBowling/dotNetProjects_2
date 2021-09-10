9e59b2cc1a4f38b1aeb6dd2584ff7161e4c2b42e
Jordan Bowling <jordanbowlingvfx@gmail.com>
2021-09-10 07:33:21 -0400

    Initial Commit

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Second time through, huh?");
+            Console.WriteLine("yuup...");
+            Console.WriteLine("don't mess it up this time.");
+            Console.ReadLine();
+        }
+    }
+}
